50f4f37ab218a98e716d4425e5da0811b70f5a65
VictorHugo1603 <vitor.hugo@newpost.com>
2021-10-09 17:18:20 -0300

    finalizando tela de login

---
+++ src/components/ButtonIcon/style.ts
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native'
+import { theme } from '../../global/styles/theme'
+
+export const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 56,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+
+        backgroundColor: theme.colors.primary,
+        borderRadius: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        
+    },
+
+    iconWrapper: {
+        width: 56,
+        height: 56,
+
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        borderRightColor: theme.colors.line,
+        borderRightWidth: 1,
+    },
+
+    icon: {
+        width: 24,
+        height: 18,
+    },
+
+    text: {
+        flex: 1,
+        color: theme.colors.heading,
+        textAlign: 'center',
+    },
+
+})
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+    colors: {
+        background: '#0d133d',
+        heading: '#dde3f0',
+        primary: '#E61C44',
+        secondary: '#495BCC',
+        line: '#991f36'
+    }
+} 
